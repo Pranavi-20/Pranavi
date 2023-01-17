47467dc418d3f3af2b785b5a7aba5811171e71b1
Pranavi <peddi.a.pranavi@capgemini.com>
2023-01-17 11:45:52 +0530

    Pranavi

---
+++ dog.java
@@ -0,0 +1,10 @@
+
+public class dog {
+	public String breed = "German Sheperd";
+	public int height = 30;
+	public int weight = 35;
+	public static void main(String[] args){
+		dog Dog = new dog();
+		System.out.println(Dog.breed);
+	}
+}
